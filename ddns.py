db72a906dde21e1fdab47fc3df6ba66adf5b0fe5
Phiwatec <52720981+Phiwatec@users.noreply.github.com>
2020-05-13 09:42:48 +0200

    Create ddns.py

---
+++ ddns.py
@@ -0,0 +1,16 @@
+
+import requests
+ipUrl='http://checkip.dyndns.com/'
+url='YourUpdateURL'
+# See DynDNS v2 by dyndns.org
+resp=requests.get(ipUrl).text
+resp=resp.split(':')
+resp=resp[1]
+resp=resp.split('<')
+resp=resp[0]
+ip=resp[1:]
+print('Your IP is: '+ip)
+
+r = requests.get(url+ip)
+print('Response from DynDNS Service')
+print(r.text)
